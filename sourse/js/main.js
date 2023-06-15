5a660ca7183da38edec3df564f2a19e120812327
Aisultan <99138717+toa1ce@users.noreply.github.com>
2023-06-15 18:41:23 +0600

    Add files via upload

---
+++ sourse/js/main.js
@@ -0,0 +1,106 @@
+const translations = {
+  en: {
+    'Sign In': 'Sign In',
+    'Unlimited movies, TV shows, and more': 'Unlimited movies, TV shows, and more',
+    'This site is just a copy of Netflix.': 'This site is just a copy of Netflix.',
+    'Ready to watch? Enter your email to create or restart your membership.': 'Ready to watch? Enter your email to create or restart your membership.',
+    'Enjoy on your TV': 'Enjoy on your TV',
+    'Watch on Smart TVs, Playstation, Xbox, Chromecast, Apple TV, Blu-ray players, and more.': 'Watch on Smart TVs, Playstation, Xbox, Chromecast, Apple TV, Blu-ray players, and more.',
+    'Watch everywhere': 'Watch everywhere',
+    'Stream unlimited movies and TV shows on your phone, tablet, laptop, and TV.': 'Stream unlimited movies and TV shows on your phone, tablet, laptop, and TV.',
+    'Create profiles for kids': 'Create profiles for kids',
+    'Send kids on adventures with their favorite characters in a space made just for them —free with your membership.': 'Send kids on adventures with their favorite characters in a space made just for them —free with your membership.',
+    'Download your shows to watch offline': 'Download your shows to watch offline',
+    'Only available on ad-free plans.': 'Only available on ad-free plans.',
+    'Frequently Asked Questions': 'Frequently Asked Questions',
+    'What is Netflix?': 'What is Netflix?',
+    'Netflix is a streaming service that offers a wide variety of award-winning TV shows, movies, anime, documentaries, and more on thousands of internet-connected devices. You can watch as much as you want, whenever you want without a single commercial – all for one low monthly price. There\'s always something new to discover and new TV shows and movies are added every week!': 'Netflix is a streaming service that offers a wide variety of award-winning TV shows, movies, anime, documentaries, and more on thousands of internet-connected devices. You can watch as much as you want, whenever you want without a single commercial – all for one low monthly price. There\'s always something new to discover and new TV shows and movies are added every week!',
+    'How much does Netflix cost?': 'How much does Netflix cost?',
+    'Watch Netflix on your smartphone, tablet, Smart TV, laptop, or streaming device, all for one fixed monthly fee. Plans range from EUR7.99 to EUR11.99 a month. No extra costs, no contracts.': 'Watch Netflix on your smartphone, tablet, Smart TV, laptop, or streaming device, all for one fixed monthly fee. Plans range from EUR7.99 to EUR11.99 a month. No extra costs, no contracts.',
+    'Where can I watch?': 'Where can I watch?',
+    'Watch anywhere, anytime. Sign in with your Netflix account to watch instantly on the web at netflix.com from your personal computer or on any internet-connected device that offers the Netflix app, including smart TVs, smartphones, tablets, streaming media players and game consoles. You can also download your favorite shows with the iOS, Android, or Windows 10 app. Use downloads to watch while you\'re on the go and without an internet connection. Take Netflix with you anywhere.': 'Watch anywhere, anytime. Sign in with your Netflix account to watch instantly on the web at netflix.com from your personal computer or on any internet-connected device that offers the Netflix app, including smart TVs, smartphones, tablets, streaming media players and game consoles. You can also download your favorite shows with the iOS, Android, or Windows 10 app. Use downloads to watch while you\'re on the go and without an internet connection. Take Netflix with you anywhere.',
+    'How do I cancel?': 'How do I cancel?',
+    'Netflix is flexible. There are no pesky contracts and no commitments. You can easily cancel your account online in two clicks. There are no cancellation fees – start or stop your account anytime.': 'Netflix is flexible. There are no pesky contracts and no commitments. You can easily cancel your account online in two clicks. There are no cancellation fees – start or stop your account anytime.',
+    'What can I watch on Netflix?': 'What can I watch on Netflix?',
+    'Netflix has an extensive library of feature films, documentaries, TV shows, anime, award-winning Netflix originals, and more. Watch as much as you want, anytime you want.': 'Netflix has an extensive library of feature films, documentaries, TV shows, anime, award-winning Netflix originals, and more. Watch as much as you want, anytime you want.',
+    'Is Netflix good for kids?': 'Is Netflix good for kids?',
+    'The Netflix Kids experience is included in your membership to give parents control while kids enjoy family-friendly TV shows and movies in their own space. Kids profiles come with PIN-protected parental controls that let you restrict the maturity rating of content kids can watch and block specific titles you don’t want kids to see.': 'The Netflix Kids experience is included in your membership to give parents control while kids enjoy family-friendly TV shows and movies in their own space. Kids profiles come with PIN-protected parental controls that let you restrict the maturity rating of content kids can watch and block specific titles you don’t want kids to see.',
+    'Get Started': 'Get Started'
+  },
+  ru: {
+    'Sign In': 'Войти',
+    'Unlimited movies, TV shows, and more': 'Фильмы, сериалы и многое другое без ограничений',
+    'This site is just a copy of Netflix.': 'Этот сайт лишь копия нетфликс.',
+    'Ready to watch? Enter your email to create or restart your membership.': 'Готовы смотреть? Введите адрес электронной почты, чтобы оформить или возобновить подписку.',
+    'Enjoy on your TV': 'Наслаждайтесь на своем телевизоре',
+    'Watch on Smart TVs, Playstation, Xbox, Chromecast, Apple TV, Blu-ray players, and more.': 'Смотрите на Smart TV, Playstation, Xbox, Chromecast, Apple TV, Blu-ray плеерах и многом другом.',
+    'Watch everywhere': 'Смотрите везде',
+    'Stream unlimited movies and TV shows on your phone, tablet, laptop, and TV.': 'Стриминг неограниченного количества фильмов и сериалов на телефоне, планшете, ноутбуке и телевизоре.',
+    'Create profiles for kids': 'Создайте профили для детей',
+    'Send kids on adventures with their favorite characters in a space made just for them —free with your membership.': 'Отправьте детей в приключения с их любимыми персонажами в специальном для них пространстве - бесплатно с вашей подпиской.',
+    'Download your shows to watch offline': 'Загружайте свои шоу для просмотра офлайн',
+    'Only available on ad-free plans.': 'Доступно только в планах без рекламы.',
+    'Frequently Asked Questions': 'Часто задаваемые вопросы',
+    'What is Netflix?': 'Что такое Netflix?',
+    'Netflix is a streaming service that offers a wide variety of award-winning TV shows, movies, anime, documentaries, and more on thousands of internet-connected devices. You can watch as much as you want, whenever you want without a single commercial – all for one low monthly price. There\'s always something new to discover and new TV shows and movies are added every week!': 'Netflix - это стриминговый сервис, предлагающий широкий выбор награжденных сериалов, фильмов, аниме, документальных фильмов и многого другого на тысячах подключенных к интернету устройств. Вы можете смотреть столько, сколько хотите, когда хотите, без рекламы - всего за низкую месячную плату. Всегда есть что-то новое для открытия, и новые сериалы и фильмы добавляются каждую неделю!',
+    'How much does Netflix cost?': 'Сколько стоит Netflix?',
+    'Watch Netflix on your smartphone, tablet, Smart TV, laptop, or streaming device, all for one fixed monthly fee. Plans range from EUR7.99 to EUR11.99 a month. No extra costs, no contracts.': 'Смотрите Netflix на своем смартфоне, планшете, Smart TV, ноутбуке или устройстве для стриминга всего за фиксированную ежемесячную плату. Планы стоят от 7,99 евро до 11,99 евро в месяц. Нет дополнительных расходов, нет контрактов.',
+    'Where can I watch?': 'Где я могу смотреть?',
+    'Watch anywhere, anytime. Sign in with your Netflix account to watch instantly on the web at netflix.com from your personal computer or on any internet-connected device that offers the Netflix app, including smart TVs, smartphones, tablets, streaming media players and game consoles. You can also download your favorite shows with the iOS, Android, or Windows 10 app. Use downloads to watch while you\'re on the go and without an internet connection. Take Netflix with you anywhere.': 'Смотрите везде, в любое время. Войдите в свою учетную запись Netflix, чтобы мгновенно смотреть на веб-сайте netflix.com с вашего персонального компьютера или на любом подключенном к интернету устройстве, предлагающем приложение Netflix, включая Smart TV, смартфоны, планшеты, устройства для потокового вещания и игровые консоли. Вы также можете скачать свои любимые шоу с помощью приложения для iOS, Android или Windows 10. Используйте загрузки для просмотра во время поездки или без подключения к интернету. Возьмите Netflix с собой куда угодно.',
+    'How do I cancel?': 'Как отменить подписку?',
+    'Netflix is flexible. There are no pesky contracts and no commitments. You can easily cancel your account online in two clicks. There are no cancellation fees – start or stop your account anytime.': 'Netflix гибкий. Нет никаких досадных контрактов и обязательств. Вы можете легко отменить подписку онлайн в два клика. Нет платы за отмену - начинайте или останавливайте свою подписку в любое время.',
+    'What can I watch on Netflix?': 'Что можно посмотреть на Netflix?',
+    'Netflix has an extensive library of feature films, documentaries, TV shows, anime, award-winning Netflix originals, and more. Watch as much as you want, anytime you want.': 'Netflix располагает обширной библиотекой художественных фильмов, документальных фильмов, сериалов, аниме, награжденных оригинальных сериалов Netflix и многого другого. Смотрите столько, сколько хотите, когда хотите.',
+    'Is Netflix good for kids?': 'Подходит ли Netflix для детей?',
+    'The Netflix Kids experience is included in your membership to give parents control while kids enjoy family-friendly TV shows and movies in their own space. Kids profiles come with PIN-protected parental controls that let you restrict the maturity rating of content kids can watch and block specific titles you don’t want kids to see.': 'Детский режим Netflix включен в вашу подписку, чтобы дать родителям контроль, пока дети наслаждаются дружелюбными семейными сериалами и фильмами в собственном пространстве. Детские профили обладают родительским контролем с пин-кодом, который позволяет ограничить рейтинг контента, который могут смотреть дети, и блокировать определенные названия, которые вы не хотите, чтобы дети видели.',
+    'Get Started': 'Начать'
+  }
+};
+const customSelect = document.querySelector('.custom-select');
+const selectedOption = customSelect.querySelector('.selected-option');
+const languageSelect = customSelect.querySelector('select');
+
+// Обработчик события изменения языка
+languageSelect.addEventListener('change', function() {
+    const selectedOptionText = languageSelect.options[languageSelect.selectedIndex].text;
+    const selectedOptionImage = languageSelect.options[languageSelect.selectedIndex].getAttribute('data-image');
+    const selectedLang = this.value;
+
+    // Обновляем текстовые элементы на выбранный язык
+    updateTranslations(selectedLang);
+
+    selectedOption.querySelector('img').src = selectedOptionImage;
+    selectedOption.querySelector('span').textContent = selectedOptionText;
+
+    // Перенаправляем на страницу с выбранным языком
+    window.location.href = '?lang=' + selectedLang;
+});
+
+// Получаем текущий язык из параметра URL
+const urlParams = new URLSearchParams(window.location.search);
+const currentLang = urlParams.get('lang');
+
+// Устанавливаем значение select элемента, если текущий язык не равен null
+if (currentLang !== null) {
+    languageSelect.value = currentLang;
+    const selectedOptionText = languageSelect.options[languageSelect.selectedIndex].text;
+    const selectedOptionImage = languageSelect.options[languageSelect.selectedIndex].getAttribute('data-image');
+    selectedOption.querySelector('img').src = selectedOptionImage;
+    selectedOption.querySelector('span').textContent = selectedOptionText;
+
+    // Обновляем текстовые элементы на текущий язык
+    updateTranslations(currentLang);
+}
+
+// Функция для обновления текстовых элементов на выбранный язык
+function updateTranslations(lang) {
+    const translatedTexts = translations[lang];
+    const textElements = document.querySelectorAll('.translate');
+
+    textElements.forEach(function(element) {
+        const key = element.dataset.translationKey;
+        if (translatedTexts.hasOwnProperty(key)) {
+            element.textContent = translatedTexts[key];
+        }
+    });
+}
